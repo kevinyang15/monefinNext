5cf62f1fd6da89a5591c74d3c969cedba40d8b21
kyang <kevinyyang15@gmail.com>
2024-08-13 20:35:40 -0300

    correccion next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
-module.exports = {
+// module.exports = {
+//     reactStrictMode: true,
+//     images: {
+//       domains: ['localhost'], // Agrega aquí tu dominio si es necesario
+//     },
+//     output: 'export',
+//     exportPathMap: async function (
+//       defaultPathMap,
+//       { dev, dir, outDir, distDir, buildId }
+//     ) {
+//       return {
+//         ...defaultPathMap,
+//       };
+//     },
+//   };
+  
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = async () => {
+  const imageminMozjpeg = await import('imagemin-mozjpeg');
+  return withPlugins([
+    [optimizedImages, {
+      /* configuración para next-optimized-images */
+      handleImages: ['jpeg', 'png', 'svg', 'webp', 'gif'],
+      optimizeImagesInDev: false,
+      mozjpeg: {
+        quality: 80,
+      },
+      imageminMozjpeg: imageminMozjpeg.default({ quality: 80 }),
+    }],
+  ], {
     reactStrictMode: true,
     images: {
       domains: ['localhost'], // Agrega aquí tu dominio si es necesario
@@ -12,6 +43,5 @@ module.exports = {
         ...defaultPathMap,
       };
     },
-  };
-  
-  
+  });
+};
